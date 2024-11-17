84242bb006e03c09bc13019d88bd8ae7ce7c5f7b
Volo1212 <valentin.fuchs@gmx.at>
2024-11-17 15:52:22 +0100

    Username/LoggedIn as member variable Deleted usernameValid function

--- Server/server.h
+++ Server/server.h
@@ -18,10 +18,11 @@ public:
 
 private:
     int attempted_logins_cnt;
+    bool loggedIn=false;
+    std::string authenticatedUser;
     int port;
     int socket_fd;
     MailManager mail_manager;
-    std::filesystem::path mail_directory;
 
     void init_socket();
     void listen_for_connections();
@@ -29,10 +30,6 @@ private:
     void resizeBuffer(char *&buffer, ssize_t& currentSize, ssize_t newCapacity);
     bool checkContentLengthHeader(std::string &contentLengthHeader, int &contentLength);
     void handle_login(int consfd, const std::string &buffer, std::string &username, bool &loggedIn);
-    // void handle_list(int consfd, const std::string &authenticatedUser, sem_t *sem);
-    // void handle_send(int consfd, const std::string &buffer, const std::string &authenticatedUser, sem_t *sem);
-    // void handle_read(int consfd, const std::string &buffer, const std::string &authenticatedUser, sem_t *sem);
-    // void handle_delete(int consfd, const std::string &buffer, const std::string &authenticatedUser, sem_t *sem);
     const bool is_valid_username(const std::string &name);
     const void send_error(const int consfd, const std::string errorMessage);
 };
